69527e576657d29929b9b0aac0f8bbf67712c13f
Paarkzb <zorikto497@gmail.com>
2022-02-18 16:31:57 +0800

    created basic map and player with basic collision

---
+++ src/level.py
@@ -0,0 +1,47 @@
+import pygame
+from settings import *
+from tile import Tile
+from player import Player
+from debug import debug
+
+class Level:
+    def __init__(self):
+        
+        #get the display surface
+        self.display_surface = pygame.display.get_surface()
+
+        # sprite group setup
+        self.visible_sprites = YSortCameraGroup()
+        self.obstacles_sprites = pygame.sprite.Group()
+
+        # sprite setup
+        self.create_map()
+
+    def create_map(self):
+        for row_index, row in enumerate(WORLD_MAP):
+            for col_index, col in enumerate(row):
+                x = col_index * TILESIZE
+                y = row_index * TILESIZE
+                if col == 'x':
+                    Tile((x, y), [self.visible_sprites, self.obstacles_sprites])
+                if col == 'p':
+                    self.player = Player((x, y), [self.visible_sprites], self.obstacles_sprites)
+
+    def run(self):
+        #update and draw the game
+        self.visible_sprites.custom_draw()
+        self.visible_sprites.update()
+        debug(self.player.direction)
+
+
+class YSortCameraGroup(pygame.sprite.Group):
+    def __init__(self):
+
+        # general setup
+        super().__init__()
+        self.display_surface = pygame.display.get_surface()
+
+
+    def custom_draw(self):
+        for sprite in self.sprites():
+            self.display_surface.blit(sprite.image, sprite.rect)
